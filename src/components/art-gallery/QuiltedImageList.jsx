8bd765c999cda98dbacfeba04da6b587ae64f0f0
Sung Woo Oh <sungbr95@gmail.com>
2021-01-26 22:57:05 -0500

    removed two photos

--- src/components/art-gallery/QuiltedImageList.jsx
+++ src/components/art-gallery/QuiltedImageList.jsx
@@ -1,8 +1,8 @@
-import * as React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import ImageList from "@material-ui/core/ImageList";
-import ImageListItem from "@material-ui/core/ImageListItem";
-import ImageListItemBar from "@material-ui/core/ImageListItemBar";
+import * as React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import ImageList from '@material-ui/core/ImageList';
+import ImageListItem from '@material-ui/core/ImageListItem';
+import ImageListItemBar from '@material-ui/core/ImageListItemBar';
 
 const useStyles = makeStyles({
   root: {
@@ -11,7 +11,7 @@ const useStyles = makeStyles({
     margin: -20,
   },
   icon: {
-    color: "rgba(255, 255, 255, 0.54)",
+    color: 'rgba(255, 255, 255, 0.54)',
   },
 });
 
@@ -40,69 +40,58 @@ export default function TitlebarBelowImageList() {
 
 const itemData = [
   {
-    img: "https://github.com/sungw5/imgs/blob/main/catface.jpg?raw=true",
-    title: "CatFace drawing",
-    author: "Sung Woo Oh",
+    img: 'https://github.com/sungw5/imgs/blob/main/catface.jpg?raw=true',
+    title: 'CatFace drawing',
+    author: 'Sung Woo Oh',
   },
   {
-    img: "https://github.com/sungw5/imgs/blob/main/colorviolins.jpg?raw=true",
-    title: "Unique Violin",
-    author: "Sung Woo Oh",
-  },
-
-  {
-    img: "https://github.com/sungw5/imgs/blob/main/bellwordspic.jpg?raw=true",
-    title: "Bell",
-    author: "Sung Woo Oh",
-  },
-  {
-    img: "https://github.com/sungw5/imgs/blob/main/feets.jpg?raw=true",
-    title: "Union",
-    author: "Sung Woo Oh",
-  },
-  {
-    img: "https://github.com/sungw5/imgs/blob/main/glass14.jpg?raw=true",
-    title: "Physics",
-    author: "Sung Woo Oh",
-  },
-  {
-    img: "https://github.com/sungw5/imgs/blob/main/IMG_2367.jpg?raw=true",
-    title: "Po",
-    author: "Sung Woo Oh",
+    img: 'https://github.com/sungw5/imgs/blob/main/colorviolins.jpg?raw=true',
+    title: 'Unique Violin',
+    author: 'Sung Woo Oh',
   },
 
   {
-    img:
-      "https://github.com/sungw5/imgs/blob/main/koreasnowstairs.jpg?raw=true",
-    title: "Snows on stairs",
-    author: "Sung Woo Oh",
+    img: 'https://github.com/sungw5/imgs/blob/main/bellwordspic.jpg?raw=true',
+    title: 'Bell',
+    author: 'Sung Woo Oh',
+  },
+  {
+    img: 'https://github.com/sungw5/imgs/blob/main/feets.jpg?raw=true',
+    title: 'Union',
+    author: 'Sung Woo Oh',
+  },
+  {
+    img: 'https://github.com/sungw5/imgs/blob/main/glass14.jpg?raw=true',
+    title: 'Physics',
+    author: 'Sung Woo Oh',
   },
   {
     img:
-      "https://github.com/sungw5/imgs/blob/main/mirrorpaperplanenme.jpg?raw=true",
-    title: "Fight yourself",
-    author: "Sung Woo Oh",
+      'https://github.com/sungw5/imgs/blob/main/koreasnowstairs.jpg?raw=true',
+    title: 'Snows on stairs',
+    author: 'Sung Woo Oh',
   },
   {
     img:
-      "https://github.com/sungw5/imgs/blob/main/tyler%20skelcopy.jpg?raw=true",
-    title: "Unlimited",
-    author: "Sung Woo Oh",
+      'https://github.com/sungw5/imgs/blob/main/mirrorpaperplanenme.jpg?raw=true',
+    title: 'Fight yourself',
+    author: 'Sung Woo Oh',
   },
   {
     img:
-      "https://github.com/sungw5/imgs/blob/main/kiddrawingpractice.jpg?raw=true",
-    title: "Brandon",
-    author: "Sung Woo Oh",
+      'https://github.com/sungw5/imgs/blob/main/tyler%20skelcopy.jpg?raw=true',
+    title: 'Unlimited',
+    author: 'Sung Woo Oh',
   },
   {
-    img: "https://github.com/sungw5/imgs/blob/main/IMG_2287.jpg?raw=true",
-    title: "Wedding",
-    author: "Sung Woo Oh",
+    img:
+      'https://github.com/sungw5/imgs/blob/main/kiddrawingpractice.jpg?raw=true',
+    title: 'Brandon',
+    author: 'Sung Woo Oh',
   },
   {
-    img: "https://github.com/sungw5/imgs/blob/main/cadincats.jpg?raw=true",
-    title: "Four cats of Cadin",
-    author: "Sung Woo Oh",
+    img: 'https://github.com/sungw5/imgs/blob/main/cadincats.jpg?raw=true',
+    title: 'Four cats of Cadin',
+    author: 'Sung Woo Oh',
   },
 ];
